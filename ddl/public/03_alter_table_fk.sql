c782fcf90f63e1a543e7ba0693c52a7f2e551d29
Felipe-MML <felipemaras20@gmail.com>
2025-11-30 15:52:48 -0300

    alter table fk

---
+++ ddl/public/03_alter_table_fk.sql
@@ -0,0 +1,65 @@
+-- order_customer -> gelocation
+ALTER TABLE order_customer
+ADD CONSTRAINT fk_order_customer_zip
+FOREIGN KEY (zip_code_prefix)
+REFERENCES geolocation (geolocation_zip_code_prefix)
+ON UPDATE CASCADE
+ON DELETE SET NULL;
+
+-- orders -> order-customer
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customer
+FOREIGN KEY (customer_id)
+REFERENCES order_customer (customer_id)
+ON UPDATE CASCADE
+ON DELETE SET NULL;
+
+
+-- order_items -> orders
+ALTER TABLE order_items
+ADD CONSTRAINT fk_items_order
+FOREIGN KEY (order_id)
+REFERENCES orders (order_id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- order_items -> products
+ALTER TABLE order_items
+ADD CONSTRAINT fk_items_product
+FOREIGN KEY (product_id)
+REFERENCES products (product_id)
+ON UPDATE CASCADE
+ON DELETE SET NULL;
+
+
+-- order_items -> order_sellers
+ALTER TABLE order_items
+ADD CONSTRAINT fk_items_seller
+FOREIGN KEY (seller_id)
+REFERENCES order_sellers (seller_id)
+ON UPDATE CASCADE
+ON DELETE SET NULL;
+
+-- order_payments -> orders
+ALTER TABLE order_payments
+ADD CONSTRAINT fk_payments_order
+FOREIGN KEY (order_id)
+REFERENCES orders (order_id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- order_reviews -> orders
+ALTER TABLE order_reviews
+ADD CONSTRAINT fk_reviews_order
+FOREIGN KEY (order_id)
+REFERENCES orders (order_id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- procucts -> product_category_name_translation
+ALTER TABLE products
+ADD CONSTRAINT fk_products_category
+FOREIGN KEY (product_category_name)
+REFERENCES product_category_name_translation (product_category_name)
+ON UPDATE CASCADE
+ON DELETE SET NULL;
