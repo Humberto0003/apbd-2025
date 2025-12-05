64d58e2d854f1b0121facdb4f999bc88d5e04918
Humberto0003 <133170690+Humberto0003@users.noreply.github.com>
2025-12-05 05:28:22 -0300

    Add poorly optimized SQL queries for analysis

--- dml/consultas (mal otimizadas)/01_consultas_mal_otimizadas.sql
+++ dml/consultas (mal otimizadas)/01_consultas_mal_otimizadas.sql
@@ -82,7 +82,66 @@ WHERE
 ORDER BY atraso DESC
 LIMIT 10;
 
+-- Consulta 7: (Mal otimizada)
+SELECT 
+    oc.customer_unique_id,
+    SUM(op.payment_value) AS total_gasto
+FROM order_payments op
+JOIN orders o 
+    ON op.order_id = o.order_id
+JOIN order_customer oc
+    ON o.customer_id = oc.customer_id
+GROUP BY oc.customer_unique_id
+ORDER BY total_gasto DESC
+LIMIT 10;
 
+-- Consulta 8: (Mal otimizada)
+SELECT
+    oc.customer_state,
+    AVG(o.order_delivered_customer_date - o.order_delivered_carrier_date) AS tempo_medio_entrega
+FROM orders o
+JOIN order_customer oc
+    ON o.customer_id = oc.customer_id
+WHERE 
+    o.order_delivered_customer_date IS NOT NULL
+    AND o.order_delivered_carrier_date IS NOT NULL
+GROUP BY oc.customer_state
+ORDER BY tempo_medio_entrega DESC;
+
+-- Consulta 9: (Mal otimizada)
+SELECT
+    s.seller_id,
+    s.seller_city,
+    s.seller_state,
+    g.geolocation_lat AS lat_seller,
+    g.geolocation_lng AS lon_seller,
+
+    (
+        6371 * acos(
+            cos(radians(-23.5505)) 
+            * cos(radians(g.geolocation_lat)) 
+            * cos(radians(g.geolocation_lng) - radians(-46.6333))
+            + sin(radians(-23.5505)) 
+            * sin(radians(g.geolocation_lat))
+        )
+    ) AS distancia_km
+
+FROM order_sellers s
+JOIN geolocation g
+    ON s.zip_code_prefix = g.geolocation_zip_code_prefix
+
+WHERE 
+    (
+        6371 * acos(
+            cos(radians(-23.5505)) 
+            * cos(radians(g.geolocation_lat)) 
+            * cos(radians(g.geolocation_lng) - radians(-46.6333))
+            + sin(radians(-23.5505)) 
+            * sin(radians(g.geolocation_lat))
+        )
+    ) <= 50
+
+ORDER BY distancia_km;
 
 
 
